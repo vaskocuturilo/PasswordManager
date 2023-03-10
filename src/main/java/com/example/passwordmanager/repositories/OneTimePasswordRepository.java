330ddedb36b29399491ec3401ce8db09f7870921
asm <vaskocuturilo@rambler.ru>
2023-03-10 17:04:50 +0100

    DV-00009: Add functionality for delete one time password.

--- src/main/java/com/example/passwordmanager/repositories/OneTimePasswordRepository.java
+++ src/main/java/com/example/passwordmanager/repositories/OneTimePasswordRepository.java
@@ -1,6 +1,7 @@
 package com.example.passwordmanager.repositories;
 
 import com.example.passwordmanager.entity.OneTimePasswordEntity;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,8 +10,10 @@ import java.util.UUID;
 
 @Repository
 public interface OneTimePasswordRepository extends CrudRepository<OneTimePasswordEntity, UUID> {
-
     @Query("SELECT code.oneTimePasswordCode FROM OneTimePasswordEntity code WHERE code.user.id = ?1")
     Integer findByOneTimePasswordCode(final UUID id);
 
+    @Modifying
+    @Query("delete from OneTimePasswordEntity code where code.oneTimePasswordCode = :code")
+    void deleteByOneTimePasswordCode(final Integer code);
 }
